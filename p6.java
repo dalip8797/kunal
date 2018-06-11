f21bcebdafca8c52d7a1832872fed8935f7a1fe0
dalip8797 <dalip8797@gmail.com>
2018-06-11 14:34:05 +0530

    added p6 file

---
+++ p6.java
@@ -0,0 +1,11 @@
+class evenodd
+{
+public static void main(String args[])
+{
+int a=21;
+if(a%2==0)
+System .out .println("the number is even");
+else
+System .out .println("the number is  odd");
+}
+}
---
+++ p8.java
@@ -0,0 +1,19 @@
+ class pailndrom
+{
+public static void main(String args[])
+{
+int  n= 151;
+int r,sum =0,temp;
+temp = n;
+while(n > 0)
+{
+r=n%10;
+sum=(sum*10)+r;
+n=n%10;
+}
+if(temp==sum)
+System .out .println("pailndrom");
+else
+System .out .println("pailndrom");
+}
+}
